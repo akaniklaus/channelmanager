3b2b28e508651bf7cebafb3b397d79ac1f8ef356
neverovsky <neverovsky@gmail.com>
2014-10-07 13:19:31 +0400

    init empty app

---
+++ app/tests/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class ExampleTest extends TestCase
+{
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+}
---
+++ app/views/hello.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CM</title>
+</head>
+<body>
+</body>
+</html>
